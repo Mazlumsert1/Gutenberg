3f2f10385aa7ae997361be7da60252bccf08f2c8
Ebbe Nielsen <Ebski91@gmail.com>
2017-05-10 09:33:41 +0200

    #3 Changed Javadoc to fit coding standards

--- src/main/java/main/dto/Author.java
+++ src/main/java/main/dto/Author.java
@@ -12,9 +12,9 @@ public class Author {
     }
 
     /**
-     * Constructor that instantiates the Author with a name.
+     * Constructor that instantiates the author with a name.
      *
-     * @param name String The name of the Author.
+     * @param name String The name of the author.
      */
     public Author(String name) {
         this.name = name;
@@ -23,8 +23,8 @@ public class Author {
     /**
      * Constructor that instantiates the Author with an UID and a name.
      *
-     * @param UID Long The UID of the Author.
-     * @param name String The name of the Author.
+     * @param UID Long The UID of the author.
+     * @param name String The name of the author.
      */
     public Author(long UID, String name) {
         this.UID = UID;
@@ -32,27 +32,27 @@ public class Author {
     }
 
     /**
-     * Get the UID of the Author.
+     * Get the UID of the author.
      *
-     * @return Long The UID of the Author.
+     * @return Long The UID of the author.
      */
     public long getUID() {
         return UID;
     }
 
     /**
-     * Gets the name of the Author.
+     * Gets the name of the author.
      *
-     * @return String The name of the Author.
+     * @return String The name of the author.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Sets the name of the Author.
+     * Sets the name of the author.
      *
-     * @param name String The name of the Author.
+     * @param name String The name of the author.
      */
     public void setName(String name) {
         this.name = name;
