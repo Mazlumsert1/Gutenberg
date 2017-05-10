d601e27ac2e78c274e6ed2699ea7aedbda5872c3
ruvazi <Runevazi@gmail.com>
2017-05-10 11:36:29 +0200

    #5 mongo database connector created with test.

--- src/test/java/test/util/DBConnectorMongoTest.java
+++ src/test/java/test/util/DBConnectorMongoTest.java
@@ -1,18 +1,30 @@
 package test.util;
 
+import main.util.DBConnectorMongo;
+import main.util.IDBConnectorMongo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DBConnectorMongoTest {
+	private IDBConnectorMongo connector;
 
 	@Test
-	public void test() {
-		assertThat(1, is(1));
+	public void defaultConstructorTest() {
+		connector = new DBConnectorMongo();
+
+		assertThat(connector, is(notNullValue()));
+	}
+
+	@Test
+	public void testDependencyInjectionConstructor() {
+		connector = new DBConnectorMongo("uri", "user", "password", "db");
+		assertThat(connector, is(notNullValue()));
 	}
 
 }
