443ea41bf6bee3c1bcd295f8cb0437e866f82a48
tjaydkSchool <dennis.michael@jubii.dk>
2017-05-10 16:08:28 +0200

    #2 Added Javadoc to the interfaces for DBConnectorMongo and DBConnectorMySQL

--- src/main/java/main/util/IDBConnectorMongo.java
+++ src/main/java/main/util/IDBConnectorMongo.java
@@ -3,6 +3,11 @@ package main.util;
 import com.mongodb.DB;
 import com.mongodb.client.MongoDatabase;
 
+/***
+ * Interface for the DBConnectorMongo class.
+ * 
+ * @author Dennis
+ */
 public interface IDBConnectorMongo {
     MongoDatabase getConnection();
 }
--- src/main/java/main/util/IDBConnectorMySQL.java
+++ src/main/java/main/util/IDBConnectorMySQL.java
@@ -5,6 +5,11 @@ import main.exception.ConnectionAlreadyClosedException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/***
+ * Interface for the DBConnectorMySQL class.
+ * 
+ * @author Dennis
+ */
 public interface IDBConnectorMySQL {
 
 	Connection getConnection() throws SQLException, ClassNotFoundException;
