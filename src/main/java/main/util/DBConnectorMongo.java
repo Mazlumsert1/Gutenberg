c059ba1ada2b066b0d74924b7c8de31561821af8
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-11 10:42:17 +0200

    DB Connector: Fix build issues (#7)
    * Updated interface for DB Connector (#2)
    
    * Added MySQL connector (#2)
    
    * #5 mongo database connector created with test. (#5)
    
    * #2 Added enviroment variables to travis yml
    
    * #2 Encrypted the username variables to MongoDB and MySQL
    
    * #2 Added Javadoc to the interfaces for DBConnectorMongo and DBConnectorMySQL
    
    * Fixed IDBConnector naming issue

--- src/main/java/main/util/DBConnectorMongo.java
+++ src/main/java/main/util/DBConnectorMongo.java
@@ -1,4 +1,59 @@
 package main.util;
 
-public class DBConnectorMongo implements IDBConnector {
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.Arrays;
+
+public class DBConnectorMongo implements IDBConnectorMongo {
+
+    private String uri;
+    private String user;
+    private String password;
+    private String database;
+    private MongoDatabase connection;
+
+    /**
+     * Default constructor.
+     */
+    public DBConnectorMongo() {
+        this.uri = System.getenv("MONGO_URI");
+        this.user = System.getenv("MONGO_USER");
+        this.password = System.getenv("MONGO_PWD");
+        this.database = System.getenv("MONGO_DB");
+    }
+
+    /**
+     * Dependency injector.
+     *
+     * @param uri String to the mongoDB.
+     * @param user String username to log in with.
+     * @param password String passqord to log in with.
+     * @param database String the database to connect to.
+     */
+    public DBConnectorMongo(String uri, String user, String password, String database) {
+        this.uri = uri;
+        this.user = user;
+        this.password = password;
+        this.database = database;
+    }
+
+    /**
+     * Create a mongodatabase client.
+     *
+     * @return MongoDatabase.
+     */
+    @Override
+    public MongoDatabase getConnection() {
+        MongoCredential credential = MongoCredential.createCredential(user, database, password.toCharArray());
+        MongoClient mongoClient = new MongoClient(new ServerAddress(uri), Arrays.asList(credential));
+        MongoDatabase db = mongoClient.getDatabase(database);
+
+        return db;
+    }
+
+
 }
