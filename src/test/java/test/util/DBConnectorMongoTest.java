26d0ed3dd9df6865ff4ef88230429a40face3da8
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-05-11 10:16:17 +0200

    Reverted commit

--- src/test/java/test/util/DBConnectorMongoTest.java
+++ src/test/java/test/util/DBConnectorMongoTest.java
@@ -1,30 +1,18 @@
 package test.util;
 
-import main.util.DBConnectorMongo;
-import main.util.IDBConnectorMongo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DBConnectorMongoTest {
-	private IDBConnectorMongo connector;
 
 	@Test
-	public void defaultConstructorTest() {
-		connector = new DBConnectorMongo();
-
-		assertThat(connector, is(notNullValue()));
-	}
-
-	@Test
-	public void testDependencyInjectionConstructor() {
-		connector = new DBConnectorMongo("uri", "user", "password", "db");
-		assertThat(connector, is(notNullValue()));
+	public void test() {
+		assertThat(1, is(1));
 	}
 
 }
--- src/test/java/test/util/DBConnectorMySQLTest.java
+++ src/test/java/test/util/DBConnectorMySQLTest.java
@@ -1,30 +1,18 @@
 package test.util;
 
-import main.util.DBConnectorMySQL;
-import main.util.IDBConnectorMySQL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DBConnectorMySQLTest {
 
-	private IDBConnectorMySQL connector;
-
 	@Test
-	public void testDefaultConstructor() {
-		connector = new DBConnectorMySQL();
-		assertThat(connector, is(notNullValue()));
-	}
-
-	@Test
-	public void testDependencyInjectionConstructor() {
-		connector = new DBConnectorMySQL("driver", "uri", "user", "password");
-		assertThat(connector, is(notNullValue()));
+	public void test() {
+		assertThat(1, is(1));
 	}
 
 }
