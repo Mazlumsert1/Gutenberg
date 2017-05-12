6b15213770441c903e58638b7fb90089c85b93b4
tjaydk <dennis_michael@jubii.dk>
2017-05-12 13:34:03 +0200

    Bash script for branches to Travis (#11)
    * #11 Added a deploy.sh script that chceks for branch, and imported it in travis.yml
    
    * #11 Fixed error in travis.yml
    
    * #11 Added the ./ prefix for path direction to .sh file
    
    * #11 Changed the ./ prefix to be under the script tag instead of the export
    
    * #11 Added the ./ prefix to both the chmod and in the script tag
    
    * #11 Changed before-script to before_script
    
    * #11 Added the then clause to the script
    
    * #11 Removed the semicolon at the before the then clause
    
    * #11 Removed extra squarebrackets and reinserted the semicolon
    
    * #11 Added a check for pull request so site will only be build if merge with master and not on pull request.
    
    * #11 Removed the curly braces around the TRAVIS_ checks.
    
    * #11 Tried to remove the ' to check if the varible can be read

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Check if the branch is master
+# if so it will generate the documentation
+# else it will just run test to verify build
+
+set -ev # -e tells the script to stop if error, -v prints the script before running
+
+if [ $TRAVIS_BRANCH == 'master' -a $TRAVIS_PULL_REQUEST == 'false' ]; then
+	mvn clean verify site -X;
+else
+	mvn verify;
+fi
