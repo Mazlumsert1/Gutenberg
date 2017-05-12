98506b243b773ebf197084099e3d9266269bcb93
Ebbe Nielsen <Ebski91@gmail.com>
2017-05-12 10:22:16 +0200

    Added constructor test to both DAOs #9

--- src/test/java/test/dao/BookDAOMySQLTest.java
+++ src/test/java/test/dao/BookDAOMySQLTest.java
@@ -1,17 +1,32 @@
 package test.dao;
 
+import main.dao.BookDAOMySQL;
+import main.util.DBConnectorMySQL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookDAOMySQLTest {
 
+	BookDAOMySQL dao;
+
 	@Test
-	public void test() {
-		assertThat(1, is(1));
+	public void defaultConstructorTest() {
+		dao = new BookDAOMySQL();
+
+		assertThat(dao, is(notNullValue()));
+	}
+
+	@Test
+	public void dependencyConstructorTest() {
+		DBConnectorMySQL connector = new DBConnectorMySQL();
+		dao = new BookDAOMySQL(connector);
+
+		assertThat(dao, is(notNullValue()));
 	}
 
 }
